96c2c0d531b01fe556457245523c66aabdf15ae9
NotACop20 <nb23@live.ca>
2014-11-24 15:09:03 -0800

    Added a java file

---
+++ Lab1.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("10 / 3: " + 10 / 3);
+        System.out.println("10 % 3: " + 10 % 3);
+
+    }
+}
